d5c02458e716aba39071652d7f708930855cea9b
Jesse-Leegwater <Jleegwater@live.com>
2021-07-07 14:45:21 -0700

    finished project

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out",
 ];
 
 /* 
@@ -31,3 +31,28 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+function menuMaker(menuItems) {
+  const menu = document.createElement("div");
+  const list = document.createElement("ul");
+
+  menu.classList.add("menu");
+  menu.appendChild(list);
+
+  menuItems.forEach((item) => {
+    const li = document.createElement("li");
+    list.appendChild(li);
+    li.textContent = item;
+  });
+
+  const button = document.querySelector(".menu-button");
+  button.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+const header = document.querySelector(".header");
+
+const menu = menuMaker(menuItems);
+
+header.appendChild(menu);
